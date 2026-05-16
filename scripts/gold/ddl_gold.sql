c81a5fd895a6768a9598b239b9b731f3a8a39cea
Rahul Ambiger <139382710+RahulAmbiger11@users.noreply.github.com>
2026-05-16 15:04:00 +0530

    Fix typo in customer_number column name

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -26,7 +26,7 @@ CREATE VIEW gold.dim_customers AS
 	SELECT 
 		ROW_NUMBER() OVER(ORDER BY ci.cst_id) AS customer_key, -- Surrogate key
 		ci.cst_id AS customer_id,
-		ci.cst_key AS cutstomer_number,
+		ci.cst_key AS customer_number,
 		ci.cst_firstname AS first_name,
 		ci.cst_lastname AS last_name,
 		la.cntry AS country,
